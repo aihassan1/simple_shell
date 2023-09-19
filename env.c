232bda05ecf12eb7ca8d199f438be489d8b8044c
Manarashraf10 <hamza13.7.18@gmail.com>
2023-09-19 15:18:45 -0700

    g

---
+++ env.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * check_env - check for the word "env" and prints environment variables
+ * @args: command line args.
+ * @env: environment variables.
+ *
+ * Description:
+ *  function checks if the command is "env" and prints all the environment
+ *  variables if it matches.|
+ *  It uses strcmp to compare the command with "env"
+ *  and iterates through the environment variables and print them.
+ *
+ * Return: void.
+ */
+void check_env(char **args, char **env)
+{
+int compare;
+int i = 0;
+
+if (args[0] != NULL)
+{
+compare = strcmp("env", args[0]);
+if (compare == 0)
+{
+
+while (env[i] != NULL)
+{
+printf("%s\n", env[i]);
+i++;
+}
+}
+else
+{
+return;
+}
+}
+}
+

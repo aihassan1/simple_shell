fb62e898bef2e3d7fae26c94b8128d67cebe43ba
Manarashraf10 <hamza13.7.18@gmail.com>
2023-09-19 15:08:26 -0700

    h

---
+++ check_for_exit.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * check_for_exit - checks for "exit" string
+ * @argv: command arguments to the shell
+ *
+ * Description: Checks if the command is "exit" and exits
+ *               the shell if true and handles exit arguments
+ */
+
+void check_for_exit(char **argv)
+{
+int compare;
+int exit_int;
+
+if (argv[0] != NULL)
+{
+compare = strcmp("exit", argv[0]);
+if (compare == 0)
+{
+if (argv[1] != NULL)
+{
+exit_int = string_to_int(argv[1]);
+if (exit_int == 0)
+{
+/*   printf("exit\n"); */
+exit(0);
+}
+else if (exit_int == -1)
+{
+fprintf(stderr, "%s: %s: numeric argument required \n",
+argv[0], argv[1]);
+return;
+}
+else
+{
+printf("%d\n", exit_int);
+exit(exit_int);
+}
+}
+exit(0);
+}
+}
+}
+
